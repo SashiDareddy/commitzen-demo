104a6d26feee11cb0934aff1e35e4d9dc0f1333c
Sashi <SashiDareddy@users.noreply.github.com>
2022-10-05 15:14:33 +0100

    feat: added function to multiply two sq roots

--- commitzen_demo/utils.py
+++ commitzen_demo/utils.py
@@ -4,3 +4,8 @@ from math import sqrt
 def add_sqrts(a: float, b: float) -> float:
     """Add the square roots of two numbers"""
     return sqrt(a) + sqrt(b)
+
+
+def multiply_sqrts(a: float, b: float) -> float:
+    """Multiply the square roots of two numbers"""
+    return sqrt(a) * sqrt(b)
