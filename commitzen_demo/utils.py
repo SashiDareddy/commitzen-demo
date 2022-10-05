efc607145040f39bbee1d1e0e894b18f27714cd7
Sashi <SashiDareddy@users.noreply.github.com>
2022-10-05 15:21:38 +0100

    feat: added new function to divide sq roots of two inputs

--- commitzen_demo/utils.py
+++ commitzen_demo/utils.py
@@ -9,3 +9,8 @@ def add_sqrts(a: float, b: float) -> float:
 def multiply_sqrts(a: float, b: float) -> float:
     """Multiply the square roots of two numbers"""
     return sqrt(a) * sqrt(b)
+
+
+def divide_sqrts(a: float, b: float) -> float:
+    """Divide the square roots of two numbers"""
+    return sqrt(a) * sqrt(b)
